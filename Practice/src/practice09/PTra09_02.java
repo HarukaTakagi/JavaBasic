647068e9729c1b83a64a0aa02ee708a253f994bc
HarukaTakagi <syunkoukouboku@gmail.com>
2018-04-29 00:33:42 +0900

    PTra10_03まで完了 10_04~10_06がわからない

--- Practice/src/practice09/PTra09_02.java
+++ Practice/src/practice09/PTra09_02.java
@@ -8,13 +8,15 @@ package practice09;
 
 public class PTra09_02 {
 	public static void main(String[] args) {
-		String[] array = {"Plain", "Island", "Mountain", "Swamp", "Forest"};
+		String[] array = { "Plain", "Island", "Mountain", "Swamp", "Forest" };
 
 		// ★ 配列arrayを、Util内のメソッドを用いてランダムに並び替えてください
 		// ※ Utilはインポートしても良いし、完全限定名で記述しても良いです
-
+		practice09.util.Util.shuffleArray(array);
 
 		// ★ ランダムに並び替えた後の配列を前から順に全て表示してください
-
+		for (int i = 0; i < 5; i++) {
+			System.out.println(array[i]);
+		}
 	}
 }
